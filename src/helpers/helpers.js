a1afb59cc4f3e16f0991c92e49333edfcd2010f0
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-01 18:53:50 -0400

    created search context

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,8 +1,9 @@
 export const filterArr = (arr) => {
+    console.log(arr);
     return [
         ...arr
             .reduce((a, c) => {
-                a.set(c.id, c);
+                a.set(c.id.videoId ? c.id.videoId : c.id, c);
                 return a;
             }, new Map())
             .values(),
--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,31 +1,19 @@
 import React, { useContext, useEffect } from 'react';
-import { AppContext } from '../../components/shared/context';
 import styles from './SearchPage.module.css';
 import SearchVideo from '../../components/search-video/SearchVideo';
 import SkeletonSearchVideo from './skeleton/SkeletonSearchVideo';
 import { useParams } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { v4 as uuidv4 } from 'uuid';
 import { Helmet } from 'react-helmet-async';
 import { SearchContext } from '../../context/searchContext';
 import { filterArr } from '../../helpers/helpers';
 
 const SearchPage = () => {
     const { keyword } = useParams();
-    // const { fetchVideosBySearch, searchedVideos, isLoading } =
-    //     useContext(AppContext);
 
     const { fetchVideosBySearch, videos, loading } = useContext(SearchContext);
 
-    const filteredSearchedVideos = Array.from(
-        new Set(videos?.map((video) => video.id.videoId))
-    ).map((id) => {
-        return {
-            video: videos.find((video) => video.id.videoId === id),
-        };
-    });
-
-    // const filteredVideos = filterArr(videos);
+    const filteredVideos = filterArr(videos);
 
     const fetchMoreSearchVideos = () => {
         fetchVideosBySearch(keyword);
@@ -44,18 +32,16 @@ const SearchPage = () => {
                 <div className={styles.search__content}>
                     <div className={styles.search__filter}>FILTER</div>
                     <InfiniteScroll
-                        dataLength={filteredSearchedVideos?.length}
+                        dataLength={filteredVideos?.length}
                         next={fetchMoreSearchVideos}
                         hasMore={true}
                     >
                         <div>
                             {loading
-                                ? [...new Array(20)].map(() => {
-                                      return (
-                                          <SkeletonSearchVideo key={uuidv4()} />
-                                      );
+                                ? [...new Array(20)].map((_, i) => {
+                                      return <SkeletonSearchVideo key={i} />;
                                   })
-                                : filteredSearchedVideos?.map(({ video }) => {
+                                : filteredVideos?.map((video) => {
                                       return (
                                           <SearchVideo
                                               key={video.id.videoId}
